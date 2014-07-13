73e4aceff274f3a6b7c32975fa5b3701178d8ade
ndavidovich <lyndvd@gmail.com>
2014-07-14 02:26:46 +0300

    basic project files added added markaps for panels added example of data usage

---
+++ js/app/data.js
@@ -0,0 +1,17 @@
+var data = [
+    {
+        "title": "Проезд перекрестков",
+        "description": "Описание теста",
+        "questions": [
+            {
+                "content": "Текст вопроса",
+                "img": "path/img.png",
+                "right": 1,
+                "answers": [
+                    "вариант 1",
+                    "вариант 2"
+                ]
+            }
+        ]
+    }
+];
---
+++ js/app/main.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+
+    document.getElementById("li1").innerHTML = data[0].title;
+
+};
