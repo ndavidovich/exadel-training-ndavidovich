18021103b7ffda66fcaf9a194a5e09f29cf92095
ndavidovich <lyndvd@gmail.com>
2014-07-15 00:58:58 +0300

    added dynamic data retrieval and rendering added panel switch on topic select

--- js/app/main.js
+++ js/app/main.js
@@ -1,5 +1,22 @@
 window.onload = function () {
 
-    document.getElementById("li1").innerHTML = data[0].title;
+    var ol = document.getElementById('topic_list');
+    console.log(ol);
+    for (var i = 0; i < data.length; i++) {
+        var topic = data[i];
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        a.innerHTML = topic.title;
+        a.onclick = function onclickElement() {
+            var panel1 = document.getElementById('panel-1');
+            panel1.style.display = 'none';
 
+            var panel2 = document.getElementById('panel-2');
+            panel2.style.display = '';
+
+        };
+        li.appendChild(a);
+        ol.appendChild(li);
+    }
+    console.log('the end');
 };
