73e145fee48eca119b536235f4c444e158b60253
ndavidovich <lyndvd@gmail.com>
2014-07-16 17:48:49 +0300

    Change code

--- js/app/main.js
+++ js/app/main.js
@@ -1,24 +1,29 @@
 window.onload = function () {
 
     var ol = document.getElementById('topicList');
-    console.log(ol);
 
     for (var i = 0; i < data.length; i++) {
         var topic = data[i];
         var li = document.createElement('li');
         var a = document.createElement('a');
         a.innerHTML = topic.title;
-        a.onclick = function onclickElement() {
-            var panel1 = document.getElementById('panelQuestion');
-            panel1.style.display = 'none';
-
-            var panel2 = document.getElementById('panelQuestion');
-            panel2.style.display = '';
-
-        };
         li.appendChild(a);
         ol.appendChild(li);
-    }
-    console.log('the end');
+
+        a.onclick = function onclickElement() {
+            var panel1 = document.getElementById('panelList');
+            panel1.className += " hidden";
+            var panel2 = document.getElementById('panelQuestion');
+            panel2.className = panel2.className.replace("hidden", "");
+        };
+    };
+
+    var user = {};
+    user.name = 'Vasya';
+    user.surname = 'Petrov';
+    user.name = 'Sergey';
+    delete user.name;
+    alert(user.name + ' ' + user.surname);
+
 };
 
