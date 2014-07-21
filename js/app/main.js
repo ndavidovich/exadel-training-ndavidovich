982fd904b7bb865d41dfadacf4b41e26522cac21
ndavidovich <lyndvd@gmail.com>
2014-07-21 18:21:30 +0300

    Add img, answers and button click event

--- js/app/main.js
+++ js/app/main.js
@@ -1,28 +1,67 @@
 window.onload = function () {
 
-    var panelList;
-    var panelQuestion;
     var selectedTopicIndex;
     var selectedQuestionIndex = 0;
-    var questionsAnswers = [];
+//    var questionsAnswers = [];
 
     document.getElementById('panelQuestion').classList.add('hidden');
 
-    var onSelectTopic = function (event) {
-        selectedTopicIndex = parseInt(event.target.getAttribute('index'), 10);
-        var selectedTopic = data[selectedTopicIndex];
+
+    /****  Show chosen topic  ****/
+    var showTopic = function (selectedTopicIndex) {
 
         document.getElementById('panelList').classList.add('hidden');
         document.getElementById('panelQuestion').classList.remove('hidden');
 
+        var selectedTopic = data[selectedTopicIndex];
         document.getElementById('topicTitle').textContent = selectedTopic.title;
+        showQuestion(selectedTopic, selectedQuestionIndex);
+
+
+        function showQuestion (selectedTopic, selectedQuestionIndex) {
+
+        /**** Show question text and question number ****/
         var selectedQuestion = selectedTopic.questions[selectedQuestionIndex];
         document.getElementById('question').textContent = selectedQuestion.question;
+        document.getElementById('questionNumber').textContent = selectedQuestionIndex + 1;
 
-        document.getElementById('questionNumber').textContent =selectedQuestionIndex + 1;
-        document.getElementById('questionImg').setAttribute('src', selectedQuestion.questionImg);
-    };
+        /**** Show image ****/
+        if (selectedQuestion.questionImg) {
+            var img = document.createElement('img');
+            img.src = selectedQuestion.questionImg;
+            document.getElementById('panelQuestion').insertBefore(img,questionAnswers);
+        }
 
+        /**** Show answers ****/
+        var answers = selectedQuestion.answers;
+        for (var i = 0; i < answers.length; i++) {
+            sepAnswer = (answers[i]);
+            var li = document.createElement('li');
+            li.textContent = sepAnswer;
+            questionAnswers.appendChild(li);
+        }
+
+        /**** Move to the next question ****/
+        function nextQuestion() {
+            selectedQuestionIndex = selectedQuestionIndex + 1;
+            if (img) {
+                panelQuestion.removeChild(img);
+            }
+            questionAnswers.innerHTML = null;
+            showQuestion(selectedTopic, selectedQuestionIndex);
+        }
+
+        document.getElementById('sendBtn').onclick = nextQuestion;
+
+    }
+    }
+    /**** Choose topic ****/
+    var onSelectTopic = function (event) {
+        selectedTopicIndex = parseInt(event.target.getAttribute('index'), 10);
+        showTopic(selectedTopicIndex);
+    }
+
+    /**** Show first widget with list of exams topics ****/
     var ol = document.getElementById('topicList');
     for (var i = 0; i < data.length; i++) {
         var topic = data[i];
